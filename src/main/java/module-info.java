03f3372143d97665ae59cabe79c9bf3dcb574250
jorge <jorgei.acostaa@uqvirtual.edu.co>
2024-04-27 20:46:24 -0500

    Se hicieron cambios en la configuracion inicial

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module coeduuniquindioparcial1fx.coeduuniquindioparcial2 {
+module co.edu.uniquindio.parcial2 {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens co.edu.uniquindio.parcial2 to javafx.fxml;
+    opens co.edu.uniquindio.parcial2.viewController to javafx.fxml;
     exports co.edu.uniquindio.parcial2;
 }
