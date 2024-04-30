4a430032a4ea26413a2d7ab78d9a9d877f15a290
ManuelQZ <142362671+ManuelQZ@users.noreply.github.com>
2024-04-29 23:04:15 -0500

    Creacion carpeta patrones
    Se crean las carpetas y el app para cada patron

---
+++ src/main/java/co/edu/uniquindio/parcial2/patrones/factoryMethod/App.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.parcial2.patrones.factoryMethod;
+
+public class App {
+
+
+
+
+}
---
+++ src/main/java/co/edu/uniquindio/parcial2/patrones/prototype/App.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.parcial2.patrones.prototype;
+
+public class App {
+}
